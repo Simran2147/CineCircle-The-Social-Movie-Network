79302ec228c4ead96213f8ea80b92726e7eb6567
avag17 <avanishagarg@gmail.com>
2024-04-04 04:07:16 -0300

    update

--- select_genre.py
+++ select_genre.py
@@ -2,35 +2,30 @@ import streamlit as st
 
 st.title("CineCircle")
 
-# Define genres and their corresponding images
-genres = {
-    "Fantasy":"images/thriller.webp",
-    "Action":"images/action.jpg",
-    "Drama":"images/drama.jpg",
-    "Adventure":"images/adventure.jpg",
-    "Comedy":"images/comedy.jpg",
-    "Crime and Mystery":"images/crime.jpg",
-    "Historical":"images/historical.jpg",
-    "Horror":"images/horror.jpg",
-    "Romance":"images/romance.png",
-    "Science fiction":"images/scifi.png",
-    "Thriller":"images/thriller.webp",
-    "Animation":"images/animation.webp",
-}
+# Define genres
+genres = [
+    "Fantasy",
+    "Action",
+    "Drama",
+    "Adventure",
+    "Comedy",
+    "Crime and Mystery",
+    "Historical",
+    "Horror",
+    "Romance",
+    "Science fiction",
+    "Thriller",
+    "Animation",
+]
 
-# Display images as checkboxes
+# Display checkboxes for genres in a single line
 selected_genres = []
-genre_col_width = 150
-row_style = "display: flex; flex-wrap: wrap; justify-content: space-around;"
-
 st.write("Select your favorite genres")
+row_style = "display: flex; justify-content: center;"
+
 st.write('<div style="{}">'.format(row_style), unsafe_allow_html=True)
-for genre, image_url in genres.items():
-    selected = st.checkbox("", key=genre)
-    st.write('<div style="display: flex; flex-direction: column; align-items: center;">', unsafe_allow_html=True)
-    st.image(image_url, width=genre_col_width)
-    st.write(genre)
-    st.write('</div>', unsafe_allow_html=True)
+for genre in genres:
+    selected = st.checkbox(genre, key=genre)
     if selected:
         selected_genres.append(genre)
 st.write('</div>', unsafe_allow_html=True)
