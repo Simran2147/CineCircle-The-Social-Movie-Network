9f12d8f9ff5f536367566bb31c084b12064cbddb
avag17 <avanishagarg@gmail.com>
2024-04-04 04:55:18 -0300

    update on selectgenre

--- select_genre.py
+++ select_genre.py
@@ -1,4 +1,13 @@
+
+
+# # Get the user ID from Cognito (assuming you are using Amazon Cognito)
+# user_id = st.session_state.get('user_id')
+
+# st.write('Hello', user_id)
+
 import streamlit as st
+import boto3
+from botocore.exceptions import NoCredentialsError
 
 st.title("CineCircle")
 
@@ -20,7 +29,7 @@ genres = [
 
 # Display checkboxes for genres in a single line
 selected_genres = []
-st.write("Select your favorite genres")
+st.write("Please select at least 3 genres to continue")
 row_style = "display: flex; justify-content: center;"
 
 st.write('<div style="{}">'.format(row_style), unsafe_allow_html=True)
@@ -30,7 +39,9 @@ for genre in genres:
         selected_genres.append(genre)
 st.write('</div>', unsafe_allow_html=True)
 
-# Display selected genres in a list
-if selected_genres:
-    st.write("Selected genres:")
-    st.write(selected_genres)
+
+
+# Add a button with text "Next" which is enabled when the user has more than 3 selections
+if len(selected_genres) >= 3:
+    st.button("Next")
+
